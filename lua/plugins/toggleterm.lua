2c25c7f738727f32fd2e5f7d3c72e98e6b90d417
Chen Bin <chen.bin11@zte.com.cn>
2025-09-23 09:43:51 +0800

    add open terminal in vertical window

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,11 +4,19 @@ return {
         config = true,
         cmd = "ToggleTerm",
         keys = {
-            { "<F6>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" }
+            { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
+            { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Toggle vertical terminal" },
         },
         opts = {
             open_mapping = [[<F6>]],
             direction = "float",
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return math.floor(vim.o.columns / 2)
+                end
+            end,
             shade_filetypes = {},
             hide_numbers = true,
             insert_mappings = true,
